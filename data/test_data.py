83a65599b3bc019c0ce1850f742cfd27e09faad0
MikhailIzegov <mikhail.izegov@yandex.ru>
2024-04-05 01:56:03 +0300

    requirements.txt is added

--- data/test_data.py
+++ data/test_data.py
@@ -1,7 +1,6 @@
-
 class TestData():
 
-test_email = "autotest.selenium.test@yandex.ru"
-test_password = "qweasd123"
+    test_email = "autotest.selenium.test@yandex.ru"
+    test_password = "qweasd123"
 
-target_value_price_filter = 100000
+    target_value_price_filter = 100000
