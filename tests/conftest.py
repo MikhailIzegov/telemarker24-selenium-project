0098c5fb4a57b661122cdc0af5f0b3c10b71f832
MikhailIzegov <mikhail.izegov@yandex.ru>
2024-04-10 03:10:30 +0300

    additional options are added for optimization

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,8 @@ def load_env():
 def browser(load_env):
     options = Options()
     options.add_argument('--start-maximized')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     # options.add_argument('--headless')
     selenoid_capabilities = {
         'browserName': 'chrome',
